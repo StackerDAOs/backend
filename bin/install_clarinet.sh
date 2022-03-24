f549a2eadce7ae0bcdae2e263e4ebd77f9f61916
mpj <rrachman@pm.me>
2022-03-24 22:19:46 +0700

    CI for clarinet (#21)
    Add CI for Clarinet

---
+++ bin/install_clarinet.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y unzip wget
+
+wget -nv https://github.com/hirosystems/clarinet/releases/download/v0.27.0/clarinet-linux-x64-glibc.tar.gz -O clarinet-linux-x64.tar.gz
+tar -xf clarinet-linux-x64.tar.gz
+chmod +x ./clarinet
+mv ./clarinet /usr/local/bin
+
