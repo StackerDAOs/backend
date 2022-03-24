4c4ecbef37f43e03586af0d5a4ef67d5eee38ba9
empeje <11813607+empeje@users.noreply.github.com>
2022-03-24 22:07:46 +0700

    Fix installation targz vs zip

--- bin/check.sh
+++ bin/check.sh
@@ -3,8 +3,8 @@
 apt-get update
 apt-get install -y unzip wget
 
-wget https://github.com/hirosystems/clarinet/releases/download/v0.27.0/clarinet-linux-x64-glibc.tar.gz -O clarinet-linux-x64.zip
-unzip clarinet-linux-x64.zip -d .
+wget -nv https://github.com/hirosystems/clarinet/releases/download/v0.27.0/clarinet-linux-x64-glibc.tar.gz -O clarinet-linux-x64.tar.gz
+tar -xf clarinet-linux-x64.tar.gz
 chmod +x ./clarinet
 mv ./clarinet /usr/local/bin
 
